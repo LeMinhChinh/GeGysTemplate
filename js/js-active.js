bcaeb59129b08d04e3a98a913161a134e03ad300
LeMinhChinh <LeMinhChinh@users.noreply.github.com>
2019-12-06 10:54:09 +0700

    add class active

---
+++ js/js-active.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.item-menu').click(function(){
+        $('.menu').slideToggle(150);
+    });
+
+    $('.item-cate').click(function(){
+        $('.cate').slideToggle(150);
+    });
+
+    $('.list-pr').addClass('hide');
+    $('.list-pr').eq(0).removeClass('hide');
+    $('.cate li').eq(0).find('a').addClass('active');
+    $('.cate a').click(function(e){
+        e.preventDefault()
+        $('.cate a').removeClass('active');
+        $(this).addClass('active');
+        var id = $(this).attr('href');
+        $(id).removeClass('hide').siblings('div').addClass('hide');
+    })
+});
--- js/js-repons.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    $('.item-menu').click(function(){
-        $('.menu').slideToggle(150);
-    });
-
-    $('.item-cate').click(function(){
-        $('.cate').slideToggle(150);
-    });
-});
