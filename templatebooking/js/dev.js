285cd2f26162d8512f584e1255741972c8307359
LeMinhChinh <LeMinhChinh@users.noreply.github.com>
2020-04-25 23:21:52 +0700

    filter room

--- templatebooking/js/dev.js
+++ templatebooking/js/dev.js
@@ -7,4 +7,18 @@ $(document).ready(function(){
         e.preventDefault();
         $('.rp-popup-map').addClass('rp-map-cancel');
     })
+    
+    for(let i = 1; i <= 4; i++){
+        $('.r-title-'+i).click(function(){
+            if($('.r-down-'+i).hasClass('disabledIcon')){
+                $('.r-down-'+i).removeClass('disabledIcon')
+                $('.r-up-'+i).addClass('disabledIcon')
+                $('.r-content-'+i).removeClass('disabledIcon')
+            }else{
+                $('.r-down-'+i).addClass('disabledIcon')
+                $('.r-up-'+i).removeClass('disabledIcon')
+                $('.r-content-'+i).addClass('disabledIcon')
+            }
+        })
+    }
 })
